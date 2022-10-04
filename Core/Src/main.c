75a20d55d59ef22e145df27ac76b32343137d740
Nguyen Tien Dat <dat.nguyendatjet7410@hcmut.edu.vn>
2022-10-04 15:23:20 +0700

    exercise-7

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -57,19 +57,47 @@ void updateClockBuffer(int hour, int minute);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int TIMER_CYCLE = 10;
+
 int timer0_counter = 0;
 int timer0_flag = 0;
-int TIMER_CYCLE = 10;
 void setTimer0 ( int duration ) {
 	timer0_counter = duration / TIMER_CYCLE ;
 	timer0_flag = 0;
 }
+
+int timer_DOT_counter = 0;
+int timer_DOT_flag = 0;
+void setTimerDOT ( int duration ) {
+	timer_DOT_counter = duration / TIMER_CYCLE ;
+	timer_DOT_flag = 0;
+}
+
+int timer_clock_counter = 0;
+int timer_clock_flag = 0;
+void setTimerClock ( int duration ) {
+	timer_clock_counter = duration / TIMER_CYCLE ;
+	timer_clock_flag = 0;
+}
+
 void timer_run () {
 	if( timer0_counter > 0) {
 		timer0_counter --;
 		if( timer0_counter == 0) timer0_flag = 1;
 	}
+
+	if( timer_DOT_counter > 0) {
+		timer_DOT_counter --;
+		if( timer_DOT_counter == 0) timer_DOT_flag = 1;
+	}
+
+	if( timer_clock_counter > 0) {
+		timer_clock_counter --;
+		if( timer_clock_counter == 0) timer_clock_flag = 1;
+	}
 }
+
+
 /* USER CODE END 0 */
 
 /**
@@ -109,28 +137,39 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   int hour = 15 , minute = 8 , second = 50;
 
-  setTimer0 (1000) ;
+  setTimer0(1000);
+  setTimerDOT(500);
+
+  updateClockBuffer(hour, minute);
+  setTimerClock(1000);
   while (1)
   {
 		if( timer0_flag == 1) {
-			HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin ) ;
-			setTimer0 (2000) ;
+			HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+			setTimer0(2000);
+		}
+		if (timer_DOT_flag == 1) {
+			HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+			setTimerDOT(500);
+		}
+		if (timer_clock_flag == 1) {
+			second ++;
+			if ( second >= 60) {
+			   second = 0;
+			   minute ++;
+			}
+			if( minute >= 60) {
+			   minute = 0;
+			   hour ++;
+			}
+			if( hour >=24) {
+			   hour = 0;
+			}
+			updateClockBuffer(hour, minute);
+
+			setTimerClock(1000);
 		}
 
-		second ++;
-		if ( second >= 60) {
-		   second = 0;
-		   minute ++;
-		}
-		if( minute >= 60) {
-		   minute = 0;
-		   hour ++;
-		}
-		if( hour >=24) {
-		   hour = 0;
-		}
-		updateClockBuffer (hour, minute) ;
-		HAL_Delay (1000) ;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -267,7 +306,6 @@ int led_buffer[4] = {1, 2, 3, 4};
 
 int seven_segment_counter = 100 / MAX_LED;
 int seven_segment_EN_state = 0;
-int LED_counter = 50;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 	seven_segment_counter--;
@@ -276,12 +314,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 
 		if (index_led >= MAX_LED) index_led = 0;
 		update7SEG(index_led++);
-	}
-
-	LED_counter--;
-	if (LED_counter <= 0){
-		LED_counter = 50;
-		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	}
 	timer_run();
 }
